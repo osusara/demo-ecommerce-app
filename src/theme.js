6599eb2afb296fd3fb3d0036c1f03fe48b2a2cf5
Damsith Karunaratna <dammakaru@gmail.com>
2024-07-20 18:23:01 +0530

    Add theme file

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+export const color = {
+  background: {
+    primary: "#FFF",
+  },
+  button: {
+    primary: "#ED342A",
+    secondary: "#000",
+  },
+  accent: {
+    light: "#F1D22B",
+    dark: "#BD9F00",
+  },
+  text: {
+    primary: "#000",
+    secondary: "#ED342A",
+    accent: "#BD9F00",
+  },
+};
