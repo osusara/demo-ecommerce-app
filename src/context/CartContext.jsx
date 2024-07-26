a0b5f1d9eff73acbb2cec4d7332e9a51fad2f2f4
Damsith Karunaratna <dammakaru@gmail.com>
2024-07-26 22:48:23 +0530

    Final Cart context

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -13,7 +13,7 @@ export const CartContext = createContext(initialState);
 const cartReducer = (state, action) => {
   switch (action.type) {
     case "ADD_ITEM":
-      return addItem(state, action.payload);
+      return state; // TODO: implement add item
 
     case "REMOVE_ALL":
       return initialState;
@@ -24,37 +24,6 @@ const cartReducer = (state, action) => {
     default:
       return state;
   }
-};
-
-const addItem = (currentState, payload) => {
-  const updatedItems = [...currentState.items];
-
-  const existingItemIndex = updatedItems.findIndex(
-    (item) =>
-      item.productId === payload.productId &&
-      item.product_variant.variant === payload.product_variant.variant
-  );
-
-  if (existingItemIndex !== -1) {
-    updatedItems[existingItemIndex].quantity += payload.quantity;
-  } else {
-    updatedItems.push(payload);
-  }
-
-  const newTotalAmount =
-    currentState.totalAmount +
-    payload.product_variant.product.price * payload.quantity;
-
-  const newDeliveryFee = newTotalAmount / 10;
-  const newDiscountPercentage = newTotalAmount > 500 ? 5 : 0;
-
-  return {
-    ...currentState,
-    items: updatedItems,
-    totalAmount: newTotalAmount,
-    deliveryFee: newDeliveryFee,
-    discountPercentage: newDiscountPercentage,
-  };
 };
 
 export const CartProvider = ({ children }) => {
